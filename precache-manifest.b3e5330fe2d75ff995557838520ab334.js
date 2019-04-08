9c9d0b8e9b7d435e56386cb46b659b361d5b0310
aungpao37 <aungpao37@hotmail.com>
2019-04-08 20:16:57 +0700

    Updates

---
+++ precache-manifest.b3e5330fe2d75ff995557838520ab334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "049c3b3bb4cee385defac723e680e76e",
+    "url": "/myapp/static/media/logo.049c3b3b.png"
+  },
+  {
+    "revision": "e2de3636550e73e2ca67c58daa71c97b",
+    "url": "/myapp/static/media/PrintPreview.e2de3636.svg"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  },
+  {
+    "revision": "a26406f74559ce0be29a",
+    "url": "/myapp/static/js/main.d54a1c31.chunk.js"
+  },
+  {
+    "revision": "136a00ab4f6bd00a401d",
+    "url": "/myapp/static/js/2.e4420912.chunk.js"
+  },
+  {
+    "revision": "a26406f74559ce0be29a",
+    "url": "/myapp/static/css/main.917630a7.chunk.css"
+  },
+  {
+    "revision": "9d67263524a341a56468dfbc7ab8b5e6",
+    "url": "/myapp/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tarangson/precache-manifest.84fe81f21f2bd4d21c608a8dca9a3275.js"
+  "/myapp/precache-manifest.b3e5330fe2d75ff995557838520ab334.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/tarangson/index.html", {
+workbox.routing.registerNavigationRoute("/myapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
