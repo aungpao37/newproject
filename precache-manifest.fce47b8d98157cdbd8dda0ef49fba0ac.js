c15d84357b93eae183d3324637d9a36e64bc5ce7
aungpao37 <aungpao37@hotmail.com>
2019-04-08 14:34:19 +0700

    Updates

---
+++ precache-manifest.fce47b8d98157cdbd8dda0ef49fba0ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "049c3b3bb4cee385defac723e680e76e",
+    "url": "/tarangson/static/media/logo.049c3b3b.png"
+  },
+  {
+    "revision": "e2de3636550e73e2ca67c58daa71c97b",
+    "url": "/tarangson/static/media/PrintPreview.e2de3636.svg"
+  },
+  {
+    "revision": "ba87f0e7b95307310432",
+    "url": "/tarangson/static/js/runtime~main.067ebfa6.js"
+  },
+  {
+    "revision": "4a487dbf9ed1a8497182",
+    "url": "/tarangson/static/js/main.8e7ca059.chunk.js"
+  },
+  {
+    "revision": "136a00ab4f6bd00a401d",
+    "url": "/tarangson/static/js/2.e4420912.chunk.js"
+  },
+  {
+    "revision": "4a487dbf9ed1a8497182",
+    "url": "/tarangson/static/css/main.917630a7.chunk.css"
+  },
+  {
+    "revision": "42780e505507bbb255a976baa13cfeff",
+    "url": "/tarangson/index.html"
+  }
+];
